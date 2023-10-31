96a0e4eec78411ba1d31c8f017643f0bb2e70a2b
Yeltsin4493 <yeltsinrhp.93@gmail.com>
2023-10-31 09:23:56 -0500

    first commit

---
+++ src/auth/pages/LoginPage.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Link as RouterLink } from "react-router-dom";
+import { Google } from "@mui/icons-material";
+import { Grid, Typography, TextField, Button, Link } from "@mui/material";
+import { AuthLayout } from "../layout/AuthLayout";
+
+export const LoginPage = () => {
+  return (
+    <AuthLayout title='Login'>
+      <form>
+        <Grid container>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="correo"
+              type="email"
+              placeholder="correo@google.com"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Contraseña"
+              type="password"
+              placeholder="contraseña"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+            <Grid item xs={12} sm={6}>
+              <Button variant="contained" fullWidth>
+                Login
+              </Button>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Button variant="contained" fullWidth>
+                <Google />
+                <Typography sx={{ ml: 1 }}> Google</Typography>
+              </Button>
+            </Grid>
+          </Grid>
+
+          <Grid container direction="row" justifyContent="end">
+            <Link component={RouterLink} color="inherit" to="/auth/register">
+              Crear una cuenta
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+    </AuthLayout>
+  );
+};
---
+++ src/journal/pages/JournalPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Typography } from "@mui/material";
+
+export const JournalPage = () => {
+  return (
+    <>
+      <Typography variant="h1">JournalPage</Typography>
+    </>
+  );
+};
